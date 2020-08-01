a5d61f8091619eeb2feb02dfb4e30ef8a9dd8589
budi0710 <budi.ariyadi140886@gmail.com>
2020-08-01 15:57:13 +0700

    tabel barang

---
+++ database/migrations/2020_08_01_084327_create_barangs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('T00_M_BRG', function (Blueprint $table) {
+            $table->char('FK_BRG',7);
+            $table->string('FN_BRG',50);
+            $table->char('FK_SAT',3);
+            $table->char('FK_JENIS',3);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('T00_M_BRG');
+    }
+}
