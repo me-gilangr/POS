fa1f2bf7bc739c75be726d24a8b42890d8cfbadc
budi0710 <budi.ariyadi140886@gmail.com>
2020-08-01 15:39:03 +0700

    rancangandb

--- public/js/app.js
+++ public/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Gilang\Pribadi\PakBudiProject\POS\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Gilang\Pribadi\PakBudiProject\POS\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Latihan Laravel\NEWPOS\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Latihan Laravel\NEWPOS\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
